ef9723d966988a413fcdce8df785d47bf3781822
Xinyu Chen <xinyuchen98@gmail.com>
2022-02-08 11:55:04 -0700

    BootcampX Queries 3 - Group By

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,5 @@
+SELECT cohorts.name AS cohort_name, COUNT(*) AS student_count
+FROM cohorts JOIN students ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+HAVING COUNT(*) >= 18
+ORDER BY student_count;
