b51742ad98f235371335ed070ce11898880f1779
Xinyu Chen <xinyuchen98@gmail.com>
2022-02-09 08:17:05 -0700

    Node Postgres

---
+++ bootcamp_app/students.js
@@ -0,0 +1,31 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// Store all user terminal input into variables
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+// SQL QUERY
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
