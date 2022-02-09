993740e9421d21d3b52432324880a3a79ece76b8
Xinyu Chen <xinyuchen98@gmail.com>
2022-02-09 08:20:20 -0700

    SQL Injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,18 +1,11 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
+  user: "vagrant",
+  password: "gitt",
+  host: "localhost",
+  database: "bootcampx",
 });
-
-// Store all user terminal input into variables
-const cohortName = process.argv[2];
-const limit = process.argv[3] || 5;
-
-// Store all potentially malicious values in an array.
-const values = [`%${cohortName}%`, limit];
 
 const queryString = `
   SELECT students.id as student_id, students.name as name, cohorts.name as cohort
@@ -22,10 +15,18 @@ const queryString = `
   LIMIT $2;
   `;
 
-// SQL QUERY
-pool.query(queryString, values)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool
+  .query(queryString, values)
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch((err) => console.error("query error", err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,30 +1,28 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
+  user: "vagrant",
+  password: "gitt",
+  host: "localhost",
+  database: "bootcampx",
 });
 
-// Store all user terminal input into variables
-const cohortName = process.argv[2];
-const values = [`${cohortName}`];
-
 const queryString = `
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = $1
-ORDER BY teacher; 
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
 `;
 
-// SQL QUERY
-pool.query(queryString, values)
-.then(res => {
-  res.rows.forEach(row => {
+const cohortName = process.argv[2] || 'JUL02';
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values).then((res) => {
+  res.rows.forEach((row) => {
     console.log(`${row.cohort}: ${row.teacher}`);
-  })
+  });
 });
